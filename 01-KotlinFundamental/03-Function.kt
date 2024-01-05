e983c859e268b11fe591effc05362fd16c4f124f
Muhamad Ivan Fadillah <70643310+ivantendou@users.noreply.github.com>
2024-01-05 18:03:16 +0700

    Create 03-Function.kt

---
+++ 01-KotlinFundamental/03-Function.kt
@@ -0,0 +1,14 @@
+// 03 - Function
+
+/*
+  function structure:
+
+  fun functionName(param1: Type1, param2: Type2, ...): ReturnType {
+    return result
+  }
+*/
+
+// function example
+fun setUser(name: String, age: Int): String {
+    return "Your name is $name, and you $age years old"
+}
