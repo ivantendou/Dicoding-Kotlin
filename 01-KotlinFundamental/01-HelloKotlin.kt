8a0c0a7a1d217eca60a5465a2df1fa1a0b7a3649
Muhamad Ivan Fadillah <70643310+ivantendou@users.noreply.github.com>
2024-01-05 17:46:13 +0700

    Create 01-HelloKotlin.kt

---
+++ 01-KotlinFundamental/01-HelloKotlin.kt
@@ -0,0 +1,17 @@
+// 01 - Hello Kotlin Program
+
+/*
+  This is how we show "Hello World!" in Kotlin. But, I change it to "Hello Kotlin!"
+  By the way, this is the multiline comment.
+*/
+
+// this is single line comment.
+
+// main function
+fun main() {
+  // we call println function to print the argument.
+   println("Hello Kotlin!")
+
+   // we can use print function too, unlike the println which print lines after the argument, print just print the argument.
+   print("Hello Kotlin")
+}
