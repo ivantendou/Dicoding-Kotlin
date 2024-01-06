f35fe05c2ee0ccbe50ff377321941efec5786d2f
Muhamad Ivan Fadillah <70643310+ivantendou@users.noreply.github.com>
2024-01-06 14:50:36 +0700

    Create 04-WhileDoWhile.kt

---
+++ 02-ControlFlow/04-WhileDoWhile.kt
@@ -0,0 +1,40 @@
+// 04 - While and Do While
+
+// While
+fun main() {
+    var counter = 1
+    while (counter <= 7){
+        println("Hello, World!")
+        counter++
+    }
+}
+/*
+   output :
+       Hello, World!
+       Hello, World!
+       Hello, World!
+       Hello, World!
+       Hello, World!
+       Hello, World!
+       Hello, World!
+*/
+
+// Do While
+fun main() {
+    var counter = 1
+    do {
+        println("Hello, World!")
+        counter++
+    } while (counter <= 7)
+}
+ 
+/*
+   output:
+       Hello, World!
+       Hello, World!
+       Hello, World!
+       Hello, World!
+       Hello, World!
+       Hello, World!
+       Hello, World!
+*/
