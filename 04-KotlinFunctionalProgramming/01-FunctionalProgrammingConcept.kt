dca603f94bfbf8a63a62087a9869f4afdacd4db3
Muhamad Ivan Fadillah <70643310+ivantendou@users.noreply.github.com>
2024-01-06 17:52:17 +0700

    Update 01-FunctionalProgrammingConcept.kt

--- 04-KotlinFunctionalProgramming/01-FunctionalProgrammingConcept.kt
+++ 04-KotlinFunctionalProgramming/01-FunctionalProgrammingConcept.kt
@@ -1,1 +1,24 @@
 // 01 - Functional Programming Concept
+
+/*
+  Functional programming is a programming paradigm that treats computation as the evaluation of mathematical functions
+  and avoids changing-state and mutable data
+*/
+
+// without functional programming style
+val list = getListUser()
+ 
+fun getUsername(): List<String>{
+    val name = mutableListOf<String>()
+    for (user in list){
+        name.add(user.name)
+    }
+    return name
+}
+
+// functional programming style
+fun getUsername(): List<String>{
+    return list.map {
+        it.name
+    }
+}
