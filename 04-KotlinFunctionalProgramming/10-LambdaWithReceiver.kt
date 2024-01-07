2e727903cd8d99d6907ae6cb705f9831c3658958
Muhamad Ivan Fadillah <70643310+ivantendou@users.noreply.github.com>
2024-01-07 17:41:30 +0700

    Update 10-LambdaWithReceiver.kt

--- 04-KotlinFunctionalProgramming/10-LambdaWithReceiver.kt
+++ 04-KotlinFunctionalProgramming/10-LambdaWithReceiver.kt
@@ -1,1 +1,31 @@
+// 10 - Lambda With Receiver
 
+fun buildString(): String {
+    val stringBuilder = StringBuilder()
+    stringBuilder.append("Hello ")
+    stringBuilder.append("from ")
+    stringBuilder.append("lambda")
+    return stringBuilder.toString()
+}
+
+// the code above is not flexible and so long
+// solution:
+fun main() {
+    val message = buildString {
+        append("Hello ")
+        append("from ")
+        append("lambda ")
+    }
+ 
+    println(message)
+}
+ 
+/*
+   output : Hello from lambda
+*/
+ 
+fun buildString(action: StringBuilder.() -> Unit): String {
+    val stringBuilder = StringBuilder()
+    stringBuilder.action()
+    return stringBuilder.toString()
+}
