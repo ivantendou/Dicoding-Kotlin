be7370e4ccef39ef8b417aab0ebd171c7d8c6827
Muhamad Ivan Fadillah <70643310+ivantendou@users.noreply.github.com>
2024-01-07 14:01:24 +0700

    Update 02-FunctionAnatomy.kt

--- 04-KotlinFunctionalProgramming/02-FunctionAnatomy.kt
+++ 04-KotlinFunctionalProgramming/02-FunctionAnatomy.kt
@@ -1,1 +1,25 @@
+// 02 - Function Anatomy
 
+private fun sum(valueA: Int, valueB: Int): Int {
+  return valueA + valueB
+}
+
+/*
+  - function header:
+  private fun sum(valueA: Int, valueB: Int): Int
+
+  - function body:
+  return valueA + valueB
+
+  - visibility modifier:
+  private
+
+  - function name
+  fun sum
+
+  - function parameter
+  (valueA: Int, valueB: Int)
+
+  - function return type
+  : Int
+*/
