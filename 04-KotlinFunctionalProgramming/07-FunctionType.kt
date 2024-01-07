538d4b30315fec5441580b60da51a75e94c38f4e
Muhamad Ivan Fadillah <70643310+ivantendou@users.noreply.github.com>
2024-01-07 16:20:58 +0700

    Update 07-FunctionType.kt

--- 04-KotlinFunctionalProgramming/07-FunctionType.kt
+++ 04-KotlinFunctionalProgramming/07-FunctionType.kt
@@ -1,1 +1,15 @@
+// 07 - Function Type
 
+typealias Arithmetic = (Int, Int) -> Int
+
+fun main() {
+    val sum: Arithmetic = { valueA, valueB -> valueA + valueB } // function as data type
+ 
+    val multiply: Arithmetic = { valueA, valueB -> valueA * valueB }
+
+    val sumResult = sum(10, 10)
+    val multiplyResult = multiply(20, 20)
+
+    println(sumResult)
+    println(multiplyResult)
+}
