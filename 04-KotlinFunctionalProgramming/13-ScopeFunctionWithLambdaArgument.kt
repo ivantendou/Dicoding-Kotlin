21f08916d65feb588d9ba112d0bc5e7838c5fee8
Muhamad Ivan Fadillah <70643310+ivantendou@users.noreply.github.com>
2024-01-07 18:28:12 +0700

    Update 13-ScopeFunctionWithLambdaArgument.kt

--- 04-KotlinFunctionalProgramming/13-ScopeFunctionWithLambdaArgument.kt
+++ 04-KotlinFunctionalProgramming/13-ScopeFunctionWithLambdaArgument.kt
@@ -1,1 +1,31 @@
+// 13 - Scope Function with Lambda Argument
 
+// let
+fun main() {
+    val message: String? = null
+    message?.let {
+        val length = it.length * 2
+        val text = "text length $length"
+        println(text)
+    } ?: run { // if null
+        val text = "message is null"
+        println(text)
+    }
+}
+
+
+// also
+fun main() {
+    val text = "Hello Kotlin"
+    val result = text.also {
+        println("value length -> ${it.length}")
+    }
+ 
+    println("text -> $result")
+}
+ 
+/*
+   output :
+           value length -> 12
+           text -> Hello Kotlin
+*/
