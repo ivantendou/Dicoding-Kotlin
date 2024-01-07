df95d27c559d38037557f5f097bf71f6e36350ab
Muhamad Ivan Fadillah <70643310+ivantendou@users.noreply.github.com>
2024-01-07 16:36:33 +0700

    Update 09-HigherOrderFunction.kt

--- 04-KotlinFunctionalProgramming/09-HigherOrderFunction.kt
+++ 04-KotlinFunctionalProgramming/09-HigherOrderFunction.kt
@@ -1,1 +1,39 @@
+// 09 - Higher Order Function
 
+// before
+fun main() {
+    printResult(10 ,sum)
+}
+ 
+fun printResult(value: Int, sum: (Int) -> Int) {
+    val result = sum(value)
+    println(result)
+}
+ 
+var sum: (Int) -> Int = { value -> value + value }
+ 
+/*
+   output : 20
+*/
+
+// after
+fun main() {
+    printResult(10){ value ->
+        value + value
+    } // notice this one
+}
+ 
+fun printResult(value: Int, sum: (Int) -> Int) {
+    val result = sum(value)
+    println(result)
+}
+ 
+/*
+   output : 20
+*/
+
+/*
+  This concept is called a Higher-Order Function, which is a function that uses other functions as parameters,
+  provides a return type, or both. What needs to be noted is, if the last argument of the function is a lambda expression,
+  then the lambda expression is placed outside the parenthesis as in the code example above.
+*/
