06544b6ff98d940c7edb88000811c16095629577
Muhamad Ivan Fadillah <70643310+ivantendou@users.noreply.github.com>
2024-01-07 17:57:17 +0700

    Update 12-ScopeFunctionWithLambdaReceiver.kt

--- 04-KotlinFunctionalProgramming/12-ScopeFunctionWithLambdaReceiver.kt
+++ 04-KotlinFunctionalProgramming/12-ScopeFunctionWithLambdaReceiver.kt
@@ -1,1 +1,47 @@
+// 12 - Scope Function with Lambda Receiver
 
+// run
+fun main() {
+    val text = "Hello"
+    val result = text.run { // run
+        val from = this
+        val to = this.replace("Hello", "Kotlin")
+        "replace text from $from to $to"
+    }
+    println("result : $result")
+}
+ 
+/*
+   output : result : replace text from Hello to Kotlin
+*/
+
+
+// with
+fun main() {
+    val message = "Hello Kotlin!"
+    val result = with(message) {
+        "First character is ${this[0]}" +
+                " and last character is ${this[this.length - 1]}"
+    }
+ 
+    println(result)
+}
+ 
+/*
+   output : First character is H and last character is !
+*/
+
+
+// apply
+fun main() {
+    val message = StringBuilder().apply { // apply
+        append("Hello ") // default way: builder.append
+        append("Kotlin!") // builder.append. <- repeating .builder is not effective
+    }
+ 
+    println(message.toString())
+}
+ 
+/*
+   output : Hello Kotlin
+*/
