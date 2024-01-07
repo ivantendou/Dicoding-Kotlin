e7a22c4a6c46cde3b955bfab6c46da129f28f336
Muhamad Ivan Fadillah <70643310+ivantendou@users.noreply.github.com>
2024-01-07 14:56:37 +0700

    Update 03-NamedAndDefaultArgument.kt

--- 04-KotlinFunctionalProgramming/03-NamedAndDefaultArgument.kt
+++ 04-KotlinFunctionalProgramming/03-NamedAndDefaultArgument.kt
@@ -1,1 +1,31 @@
+// 03 - Named Argument
 
+fun main() { 
+    // named argument (first = "Kotlin" , middle = " is ", last = "Awesome")
+    val fullName = getFullName(first = "Kotlin" , middle = " is ", last = "Awesome")
+    print(fullName)
+}
+ 
+fun getFullName(first: String, middle: String, last: String): String {
+    return "$first $middle $last"
+}
+
+
+// - - -
+fun getFullName(
+        // default argument
+        first: String = "Kotlin", 
+        middle: String = " is ", 
+        last: String = "Awesome"): String {
+    return "$first $middle $last"
+}
+
+fun main() {
+    // when we call getFullName without parameter, the value will be default argument
+    val fullName = getFullName()
+    print(fullName)
+}
+ 
+/*
+   output : Kotlin is Awesome
+*/
