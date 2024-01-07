e26bcbc6db18fda349594b2d8a01d8536bdbaa1d
Muhamad Ivan Fadillah <70643310+ivantendou@users.noreply.github.com>
2024-01-07 19:17:42 +0700

    Update 18-Recursion.kt

--- 04-KotlinFunctionalProgramming/18-Recursion.kt
+++ 04-KotlinFunctionalProgramming/18-Recursion.kt
@@ -1,1 +1,35 @@
+// 18 - Recursion
 
+// without recursion
+fun factorial(n: Int): Int {
+    return if (n == 1) {
+        n
+    } else {
+        var result = 1
+        for (i in 1..n) {
+            result *= i
+        }
+        result
+    }
+ }
+
+// with recursion
+fun factorial(n: Int): Int {
+    return if (n == 1) {
+        n
+    } else {
+        n * factorial(n - 1)
+    }
+ }
+
+// cons of recustion is: Exception in thread "main" java.lang.StackOverflowError, the solution is use tail call recursion
+
+// tail call recursion
+tailrec fun factorial(n: Int, result: Int = 1): Int {
+    val newResult = n * result
+    return if (n == 1) {
+        newResult
+    } else {
+        factorial(n - 1, newResult)
+    }
+}
