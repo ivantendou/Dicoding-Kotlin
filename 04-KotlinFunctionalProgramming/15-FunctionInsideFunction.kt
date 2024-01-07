1ad8898c4d83b34a22d9c1db36b1fe0fa50e89ac
Muhamad Ivan Fadillah <70643310+ivantendou@users.noreply.github.com>
2024-01-07 18:56:53 +0700

    Update 15-FunctionInsideFunction.kt

--- 04-KotlinFunctionalProgramming/15-FunctionInsideFunction.kt
+++ 04-KotlinFunctionalProgramming/15-FunctionInsideFunction.kt
@@ -1,1 +1,21 @@
+// 15 - Function inside Function
+
+// inner funttion example
+fun setWord(message: String) {
+    fun printMessage(text: String) {
+        println(text)
+    }
+  
+    printMessage(message)
+ }
+
+// inner function can access part of outer function, for example, the parameter from setWord funciton
+fun setWord(message: String) {
+    fun printMessage() {
+        println(message)
+    }
+  
+    printMessage()
+ }
+
 
