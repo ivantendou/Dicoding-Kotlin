54b0406d9f811e670a03cf491dabab0e20005d05
Muhamad Ivan Fadillah <70643310+ivantendou@users.noreply.github.com>
2024-01-07 15:26:17 +0700

    Update 05-Extension.kt

--- 04-KotlinFunctionalProgramming/05-Extension.kt
+++ 04-KotlinFunctionalProgramming/05-Extension.kt
@@ -1,1 +1,26 @@
+// 05 - Extension
 
+// Extension Functions
+fun main() {
+    10.printInt() // (Int).printInt()
+}
+ 
+/*
+   output : value 10
+*/
+ 
+fun Int.printInt() {
+    print("value $this")
+}
+
+// Extension Properties
+fun main() {
+    println(10.slice)
+}
+ 
+/*
+   output : 5
+*/
+ 
+val Int.slice: Int
+    get() = this / 2
