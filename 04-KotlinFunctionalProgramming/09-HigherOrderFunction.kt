7843ae4eb7fd3990f7d1aa4e65ae1389a28bb4f2
Muhamad Ivan Fadillah <70643310+ivantendou@users.noreply.github.com>
2024-01-07 16:39:34 +0700

    Update 09-HigherOrderFunction.kt

--- 04-KotlinFunctionalProgramming/09-HigherOrderFunction.kt
+++ 04-KotlinFunctionalProgramming/09-HigherOrderFunction.kt
@@ -36,4 +36,15 @@ fun printResult(value: Int, sum: (Int) -> Int) {
   This concept is called a Higher-Order Function, which is a function that uses other functions as parameters,
   provides a return type, or both. What needs to be noted is, if the last argument of the function is a lambda expression,
   then the lambda expression is placed outside the parenthesis as in the code example above.
+
+  However, please note that the use of Higher-Order Functions has an impact on program performance. 
+  This is because when a Higher-Order Function is executed, behind the scenes there is a process of
+  creating many instances if it is called repeatedly.
+
+  the solution:
 */
+
+inline fun printResult(value: Int, sum: (Int) -> Int) {
+    val result = sum(value)
+    println(result)
+}
