b1733e945335b77d54f6534228bea35f19fa6a08
Muhamad Ivan Fadillah <70643310+ivantendou@users.noreply.github.com>
2024-01-08 15:56:44 +0700

    Create 07-SecondaryConstructor.kt

---
+++ 05-KotlinOOP/07-SecondaryConstructor.kt
@@ -0,0 +1,56 @@
+// 07
+
+// Secondary Constructor
+
+class Animal(name: String, weight: Double, age: Int) {
+    val name: String
+    val weight: Double
+    val age: Int
+    var isMammal: Boolean
+ 
+    init {
+        this.weight = if(weight < 0) 0.1 else weight
+        this.age = if(age < 0) 0  else age
+        this.name = name
+        this.isMammal = false
+    }
+ 
+    constructor(name: String, weight: Double, age: Int, isMammal: Boolean) : this(name, weight, age) {
+        this.isMammal = isMammal
+    }
+}
+ 
+fun main() {
+    val dicodingCat = Animal("Dicoding Miaw", 2.5, 2, true)
+    println("Nama: ${dicodingCat.name}, Berat: ${dicodingCat.weight}, Umur: ${dicodingCat.age}, mamalia: ${dicodingCat.isMammal}")
+ 
+    val dicodingBird = Animal("Dicoding tweet", 0.5, 1)
+    println("Nama: ${dicodingBird.name}, Berat: ${dicodingBird.weight}, Umur: ${dicodingBird.age}, mamalia: ${dicodingBird.isMammal}")
+}
+ 
+/*
+output:
+    Nama: Dicoding Miaw, Berat: 2.5, Umur: 2, mamalia: true
+    Nama: Dicoding tweet, Berat: 0.5, Umur: 1, mamalia: false
+*/
+
+
+// = = =
+
+// Default Constructor
+class Animal{
+    val name: String = "Dicoding Miaw"
+    val weight: Double = 4.2
+    val age: Int = 2
+    val isMammal: Boolean = true
+}
+ 
+fun main(){
+    val dicodingCat = Animal()
+    println("Nama: ${dicodingCat.name}, Berat: ${dicodingCat.weight}, Umur: ${dicodingCat.age}, mamalia: ${dicodingCat.isMammal}" )
+}
+ 
+/*
+output:
+    Nama: Dicoding Miaw, Berat: 4.2, Umur: 2, mamalia: true
+*/
