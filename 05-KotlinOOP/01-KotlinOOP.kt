ba82d38d86c25b25fd785806766ee5079a0f25c6
Muhamad Ivan Fadillah <70643310+ivantendou@users.noreply.github.com>
2024-01-08 12:51:00 +0700

    Update 01-KotlinOOP.kt

--- 05-KotlinOOP/01-KotlinOOP.kt
+++ 05-KotlinOOP/01-KotlinOOP.kt
@@ -1,1 +1,12 @@
+// 01 - Kotlin OOP Concept
+
+// object everywhere
+
+/*
+  In this code we create a variable which is also an object with the name someString. 
+  This object is a realization of the String class, so the someString object has functions and 
+  properties that are members of the String class.
+*/
+val someString = "Dicoding"
+
 
