f0626460d76fc84ee1f3a4f9b7ddfca76b603a0a
Muhamad Ivan Fadillah <70643310+ivantendou@users.noreply.github.com>
2024-01-09 09:21:30 +0700

    Create 01-ConcurrencyInKotlin.kt

---
+++ 07-Coroutine/01-ConcurrencyInKotlin.kt
@@ -0,0 +1,7 @@
+// 01 - Concurrency
+
+/* 
+  Concurrency is several processes that occur simultaneously in a system. 
+  Concurrency is a natural phenomenon that commonly occurs. Just like in the real world, many activities are carried out at the same time. 
+  Thus, when we want to develop a system to help with real activities, of course we have to care about concurrency.
+*/
